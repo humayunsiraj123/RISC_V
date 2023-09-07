1c9e04967579c1f126a3332f413b7815365d4cbb
Humayun Sheikh <humayunsiraj0@gmail.com>
2023-09-08 01:57:25 +0500

    updated 4bit alu deocoder more instr

--- alu_decoder.sv
+++ alu_decoder.sv
@@ -9,7 +9,18 @@ module alu_decoder (
 logic [1:0]op_funct7;
 
 
-  always_comb begin : proc_alu_deocder
+/*0110011 (51) 000 0000000 R add rd, rs1, rs2 add rd = rs1 + rs2
+0110011 (51) 000 0100000 R sub rd, rs1, rs2 sub rd = rs1 — rs2
+0110011 (51) 001 0000000 R sll rd, rs1, rs2 shift left logical rd = rs1 << rs24:0
+0110011 (51) 010 0000000 R slt rd, rs1, rs2 set less than rd = (rs1 < rs2)
+0110011 (51) 011 0000000 R sltu rd, rs1, rs2 set less than unsigned rd = (rs1 < rs2)
+0110011 (51) 100 0000000 R xor rd, rs1, rs2 xor rd = rs1 ^ rs2
+0110011 (51) 101 0000000 R srl rd, rs1, rs2 shift right logical rd = rs1 >> rs24:0
+0110011 (51) 101 0100000 R sra rd, rs1, rs2 shift right arithmetic rd = rs1 >>> rs24:0
+0110011 (51) 110 0000000 R or rd, rs1, rs2 or rd = rs1 | rs2
+0110011 (51) 111 0000000 R and rd, rs1, rs2 and rd = rs1 & rs2
+*/
+/*  always_comb begin : proc_alu_deocder
     op_funct7 = {op_code[5],funct7[5]};
     if(alu_op==0)
       alu_control = 3'b000;//add lw,sw instr
@@ -30,7 +41,25 @@ logic [1:0]op_funct7;
           default:alu_control =  3'bxxx;//
         endcase // {funct3,op_code[5],funct7[5]}
       end
-  end
+  end*/
+
+always_comb begin 
+casez({op_code[5],funct7[5],funct3})
+ 'b10_000 : alu_control = 4'd0;// add reg
+ 'b11_000 : alu_control = 4'd1; // sub reg
+ 'b10_001 : alu_control = 4'd; // sll  
+ 'b10_010 : alu_control = 4'd5; // slt set less than
+ 'b10_011 : alu_control = 4'd; // sltu usinged
+ 'b10_100 : alu_control = 4'd; // xor
+ 'b10_101 : alu_control = 4'd; // shift right logic
+ 'b11_101 : alu_control = 4'd; // Shift arthmetic right
+ 'b10_110 : alu_control = 4'd3; // or
+ 'b10_111 : alu_control = 4'd2; // and
+  endcase
+end
+
+
+
 
 
 
